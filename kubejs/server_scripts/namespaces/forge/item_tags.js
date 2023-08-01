2d82b1bc74507bec33351e1d34df738adc88e0ae
elysia_silly.zip <86559966+ElysiaSilly@users.noreply.github.com>
2023-08-01 08:12:58 +0200

    directory cleanup

--- kubejs/server_scripts/namespaces/forge/item_tags.js
+++ kubejs/server_scripts/namespaces/forge/item_tags.js
@@ -13,23 +13,32 @@ ServerEvents.tags('item', (event) => {
         
         event.add(`${item.tag}`, `kubejs:${metal.id}_${item.id}`)
         event.add(`${item.tag}${item.appendTag}${metal.id}`, `kubejs:${metal.id}_${item.id}`)
-        
     })})
 
     global.gemOres.forEach((gem) => {
     global.gemBlocks.forEach((block) => {
         
-        event.add(`${block.tag}${block.appendTag}`, `kubejs:${gem.id}_${block.id}`)
-        event.add(`${block.tag}${block.appendTag}${gem.id}`, `kubejs:${gem.id}_${block.id}`)
-        
+        event.add(`${block.tag}`, `kubejs:${gem.id}_${block.id}`)
+        event.add(`${block.tag}${block.appendTag}${gem.id}`, `kubejs:${gem.id}_${block.id}`)     
     })})
 
     global.gemOres.forEach((gem) => {
     global.gemItems.forEach((item) => {
             
         event.add(`${item.tag}`, `kubejs:${gem.id}_${item.id}`)
-        event.add(`${item.tag}${item.appendTag}${gem.id}`, `kubejs:${gem.id}_${item.id}`)
-            
+        event.add(`${item.tag}${item.appendTag}${gem.id}`, `kubejs:${gem.id}_${item.id}`) 
     })})
+
+    global.minerals.forEach((mineral) => {
+        
+        event.add(`forge:minerals/${mineral.id}`, `kubejs:${mineral.id}`)
+        event.add(`forge:minerals`, `kubejs:${mineral.id}`)
+    })
+
+    global.strata.forEach((strata) => {
+        
+        event.add(`forge:strata/${strata.id}`, `kubejs:${strata.id}`)
+        event.add(`forge:strata`, `kubejs:${strata.id}`)
+    })
 
 })
