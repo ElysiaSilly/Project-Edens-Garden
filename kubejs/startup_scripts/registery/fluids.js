cb8fb8fdd927e482e34c637da7aa0aa51468faa3
elysia_silly.zip <86559966+ElysiaSilly@users.noreply.github.com>
2023-08-03 02:46:24 +0200

    hrm

--- kubejs/startup_scripts/registery/fluids.js
+++ kubejs/startup_scripts/registery/fluids.js
@@ -3,11 +3,8 @@ StartupEvents.registry('fluid', event => {
     global.fluids.forEach((fluid) => {
         event
       .create(`${fluid.id}`)
-      //.thickTexture(0xFF0000)
-      //.bucketColor(0xFF0000)
-      //.displayName('Thick Fluid')
       .luminosity(fluid.luminosity)
-      .thinTexture(0x004080)
+      .thinTexture(0x004080) // Textures need to be implemented, for now just using goofy ah kubejs tinting
     })
 
 })
